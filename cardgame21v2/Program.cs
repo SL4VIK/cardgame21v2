3f6738d0cc541d2c5ba95141de29b3b83b96c67c
Сидоренко Славик <termenator2001@gmail.com>
2019-09-01 12:29:38 +0300

    2

--- cardgame21v2/Program.cs
+++ cardgame21v2/Program.cs
@@ -12,7 +12,7 @@ namespace cardgame21v2
         {
             const int b = 36;
             int[] cards = new int[b] { 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 11, 11, 11, 11 };
-            int index;
+            int index = 0;
             Random random = new Random();
             for (int i = cards.Length - 1; i >= 0; i--)
             {
@@ -22,167 +22,177 @@ namespace cardgame21v2
                 cards[i] = temp;
             }
             string NewGame = "1";
-            while(NewGame == "1")
+            Random Player = new Random();
+            while (NewGame == "1")
             {
-                Random NumPlayer = new Random();
-                var a = (NumPlayer.Next(0, 2));
+                var a = (Player.Next(0, 2));
                 if (a == 0)
                 {
-                    int summChelovek = 0;
-                    int summComputer = 0;
+                    int PointsPlayer1 = 0;
+                    int PointsPlayer2 = 0;
                     Console.WriteLine("You take cards first");
                     Console.ReadKey();
-                    summChelovek = cards[0] + cards[1];
-                    Console.WriteLine($"You took {cards[0]} and {cards[1]}, your summa = {summChelovek} of 21 ");
-                    summComputer = cards[2] + cards[3];
-                    index = 4;
+                    int q = cards[index];
+                    index++;
+                    int w = cards[index];
+                    index++;
+                    PointsPlayer1 = q + w;
+                    Console.WriteLine($"You took {q} and {w}, your summa = {PointsPlayer1} of 21 ");
+                    int e = cards[index];
+                    index++;
+                    int r = cards[index];
+                    index++;
+                    PointsPlayer2 = e + r;
                     Console.WriteLine("Computer took cards too");
-                    if (summChelovek == 21)
+                    if (PointsPlayer1 == 21)
                     {
                         Console.WriteLine("You took 21 points, you won!");
                     }
-                    if (summComputer == 21)
+                    if (PointsPlayer2 == 21)
                     {
-                        Console.WriteLine("Mashina took 21 points, you lose!");
+                        Console.WriteLine("Computer took 21 points, you lose!");
                     }
-                    if (summChelovek == 21 && summComputer == 21)
+                    if (PointsPlayer1 == 21 && PointsPlayer2 == 21)
                     {
                         Console.WriteLine("DRAW!!!");
                     }
                     Console.ReadKey();
-                    string MoreChelovek = "1";
-                    while (MoreChelovek == "1" && summChelovek < 21)
+                    string MorePlayer1 = "1";
+                    while (MorePlayer1 == "1" && PointsPlayer1 < 21)
                     {
                         Console.WriteLine("Do you want take more?");
-                        Console.WriteLine("1 - Yes / 2 - No");
-                        MoreChelovek = Console.ReadLine();
-                        if (MoreChelovek == "1")
+                        Console.WriteLine("Yes - 1 / No -click something");
+                        MorePlayer1 = Console.ReadLine();
+                        if (MorePlayer1 == "1")
                         {
-                            summChelovek += cards[index];
+                            
+                            PointsPlayer1 += cards[index];
                             index++;
-                            if (summChelovek > 21)
+                            if (PointsPlayer1 > 21)
                             {
-                                Console.WriteLine($"You took card, your summa = {summChelovek}");
+                                Console.WriteLine($"You took card, your summa = {PointsPlayer1}");
                                 Console.WriteLine("You have more than 21, you lose!");
                                 break;
                             }
-                            if (summChelovek == 21)
+                            if (PointsPlayer1 == 21)
                             {
                                 break;
                             }
-                            Console.WriteLine($"You took card, your summa = {summChelovek}");
+                            Console.WriteLine($"You took card, your summa = {PointsPlayer1}");
                         }
 
                     }
-                    while (21 - summComputer > 6)
+                    while (PointsPlayer2 < 16)
                     {
-                        summComputer += cards[index];
+                        PointsPlayer2 += cards[index];
                         index++;
-                        if (summComputer > 21)
+                        if (PointsPlayer2 > 21)
                         {
-                            Console.WriteLine($"Computer took card, his summa = {summComputer}");
+                            Console.WriteLine($"Computer took card, his summa = {PointsPlayer2}");
                             Console.WriteLine("Computer have more than 21, VICTORY!!!");
                             break;
                         }
-                        if (summComputer == 21)
+                        if (PointsPlayer2 == 21)
                         {
                             break;
                         }
                     }
-                    if (summChelovek > summComputer && summChelovek <= 21)
+                    if (PointsPlayer1 > PointsPlayer2 && PointsPlayer1 <= 21)
                     {
-                        Console.WriteLine($"You have {summChelovek} points, computer have {summComputer} pionts.");
+                        Console.WriteLine($"You have {PointsPlayer1} points, computer have {PointsPlayer2} points.");
                         Console.WriteLine("VICTORY!!!");
                     }
-                    else if (summComputer > summChelovek && summComputer <= 21)
+                    else if (PointsPlayer2 > PointsPlayer1 && PointsPlayer2 <= 21)
                     {
-                        Console.WriteLine($"You have {summChelovek} points, computer have {summComputer} pionts.");
+                        Console.WriteLine($"You have {PointsPlayer1} points, computer have {PointsPlayer2} points.");
                         Console.WriteLine("You LOSE!!!");
                     }
                 }
                 else
                 {
-                    int summChelovek = 0;
-                    int summComputer = 0;
+                    int PointsPlayer1 = 0;
+                    int PointsPlayer2 = 0;
                     Console.WriteLine("Computer take cards first");
                     Console.ReadKey();
-                    summComputer = cards[0] + cards[1];
+                    int e = cards[index];
+                    index++;
+                    int r = cards[index];
+                    index++;
+                    PointsPlayer2 = e + r;
                     Console.WriteLine("Computer took cards");
-                    summChelovek = cards[2] + cards[3];
-                    index = 4;
-                    Console.WriteLine($"You took {cards[2]} and {cards[3]}, your summa = {summChelovek} of 21 ");
-                    if (summComputer == 21)
+                    int q = cards[index];
+                    index++;
+                    int w = cards[index];
+                    index++;
+                    PointsPlayer1 = q + w;
+                    Console.WriteLine($"You took {q} and {w}, your summa = {PointsPlayer1} of 21 ");
+                    if (PointsPlayer2 == 21)
                     {
-                        Console.WriteLine("Mashina took 21 points, you lose!");
+                        Console.WriteLine("Computer took 21 points, you lose!");
                     }
-                    if (summChelovek == 21)
+                    if (PointsPlayer1 == 21)
                     {
                         Console.WriteLine("You took 21 points, you won!");
                     }
-                    if (summComputer == 21 && summChelovek == 21)
+                    if (PointsPlayer2 == 21 && PointsPlayer1 == 21)
                     {
                         Console.WriteLine("DRAW!!!");
                     }
                     Console.ReadKey();
-                    while (21 - summComputer > 6)
+                    while (21 - PointsPlayer2 > 6)
                     {
-                        summComputer += cards[index];
+                        PointsPlayer2 += cards[index];
                         index++;
-                        if (summComputer > 21)
+                        if (PointsPlayer2 > 21)
                         {
-                            Console.WriteLine($"Computer took card, his summa = {summComputer}");
+                            Console.WriteLine($"Computer took card, his summa = {PointsPlayer2}");
                             Console.WriteLine("Computer have more than 21, VICTORY!!!");
                             break;
                         }
-                        if (summComputer == 21)
+                        if (PointsPlayer2 == 21)
                         {
                             break;
                         }
                     }
-                    string MoreChelovek = "1";
-                    while (MoreChelovek == "1" && summChelovek < 21)
+                    string MorePlayer1 = "1";
+                    while (MorePlayer1 == "1" && PointsPlayer1 < 21)
                     {
                         Console.WriteLine("Do you want take more?");
-                        Console.WriteLine("1 - Yes / 2 - No");
-                        MoreChelovek = Console.ReadLine();
-                        if (MoreChelovek == "1")
+                        Console.WriteLine("Yes - 1 / No -click something");
+                        MorePlayer1 = Console.ReadLine();
+                        if (MorePlayer1 == "1")
                         {
-                            summChelovek += cards[index];
+                            PointsPlayer1 += cards[index];
                             index++;
-                            if (summChelovek > 21)
+                            if (PointsPlayer1 > 21)
                             {
-                                Console.WriteLine($"You took card, your summa = {summChelovek}");
+                                Console.WriteLine($"You took card, your summa = {PointsPlayer1}");
                                 Console.WriteLine("You have more than 21, you lose!");
                                 break;
                             }
-                            if (summChelovek == 21)
+                            if (PointsPlayer1 == 21)
                             {
                                 break;
                             }
-                            Console.WriteLine($"You took card, your summa = {summChelovek}");
+                            Console.WriteLine($"You took card, your summa = {PointsPlayer1}");
                         }
 
                     }
-                    if (summComputer > summChelovek && summComputer < 21)
+                    if (PointsPlayer2 > PointsPlayer1 && PointsPlayer2 < 21)
                     {
-                        Console.WriteLine($"You have {summChelovek} points, computer have {summComputer} pionts.");
+                        Console.WriteLine($"You have {PointsPlayer1} points, computer have {PointsPlayer2} points.");
                         Console.WriteLine("You LOSE!!!");
                     }
-                    else if (summChelovek > summComputer && summChelovek < 21)
+                    else if (PointsPlayer1 > PointsPlayer2 && PointsPlayer1 < 21)
                     {
-                        Console.WriteLine($"You have {summChelovek} points, computer have {summComputer} pionts.");
+                        Console.WriteLine($"You have {PointsPlayer1} points, computer have {PointsPlayer2} points.");
                         Console.WriteLine("VICTORY!!!");
                     }
                 }
-                Console.WriteLine("Do you want to start new game?");
+                Console.WriteLine("Do you want start new game?");
                 Console.WriteLine("1 - Yes / 2 - No");
                 NewGame = Console.ReadLine();
             }
-            if (NewGame == "2")
-            {
-                Environment.Exit(0);
-            }
-            Console.ReadKey();
         }
     }
 }
